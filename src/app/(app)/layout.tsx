8414ed66793ba5d6257faa4a6830f6a387c92fdb
Abhay Singh <abhising0608@gmail.com>
2025-12-30 11:22:15 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,20 +4,23 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { AppHeader } from '@/components/app-header';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { AuthProvider } from '@/hooks/use-auth';
+import { FirebaseClientProvider } from '@/firebase';
 
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <AppHeader />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
-            {children}
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
+      <FirebaseClientProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <AppHeader />
+            <main className="flex-1 p-4 bg-background">
+              {children}
+            </main>
+          </SidebarInset>
+        </SidebarProvider>
+      </FirebaseClientProvider>
     </AuthProvider>
   );
 }
