a9f3cfc29506d0cc0320157a29adc5e410353e1f
Abhay Singh <abhising0608@gmail.com>
2026-01-11 11:20:18 +0000

    Remove the entire Vertical Work Flow journey from the overall applicatio

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,13 +4,11 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { AppHeader } from '@/components/app-header';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { WorkflowProvider } from '@/context/workflow-context';
-import { WorkflowStepper } from '@/components/workflow-stepper';
 import { usePathname } from 'next/navigation';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const showStepper = /^\/(company-information|operational-input|due-diligence|manage-instrument|notes\/new|rating-note)\/.+/.test(pathname);
-
+  
   return (
       <WorkflowProvider>
         <SidebarProvider>
@@ -18,8 +16,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <SidebarInset>
             <AppHeader />
             <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
-              {showStepper && <WorkflowStepper />}
-              <div className={showStepper ? "mt-8" : ""}>
+              <div>
                 {children}
               </div>
             </main>
