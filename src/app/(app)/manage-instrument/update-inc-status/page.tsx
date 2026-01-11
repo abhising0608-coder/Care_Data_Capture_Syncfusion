36daaaef463f5db16a8272bb317d131d1d908275
Abhay Singh <abhising0608@gmail.com>
2026-01-11 10:55:35 +0000

    Addendum_RatingWorkflow_11:   Manage Instrument – Update INC status Scre

--- src/app/(app)/manage-instrument/update-inc-status/page.tsx
+++ src/app/(app)/manage-instrument/update-inc-status/page.tsx
@@ -130,7 +130,7 @@ export default function UpdateIncStatusPage() {
                                                     </FormControl>
                                                 </PopoverTrigger>
                                                 <PopoverContent className="w-auto p-0" align="start">
-                                                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} />
+                                                    <Calendar mode="single" selected={field.value ?? undefined} onSelect={field.onChange} />
                                                 </PopoverContent>
                                             </Popover>
                                             <FormMessage />
